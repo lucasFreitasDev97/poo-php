9e5f54bf652f6826c11eb5de2b8fa9bc7e9b78e5
lucasFreitasDev97 <lukasm2@live.com>
2024-09-09 15:31:14 -0300

    exercicio 03 - cadastro funcionarios

---
+++ exercicios/encapsulamento/03_cadastro_funcionarios.php
@@ -0,0 +1,42 @@
+<?php
+
+class Funcionario{
+
+    private string $nome;
+    private string $cargo;
+    private float $salario;
+
+    public function __construct(string $nome, string $cargo, float $salario)
+    {
+        $this->nome = $nome;
+        $this->cargo = $cargo;
+        $this->salario = $salario;
+    }
+
+    public function alterarCargo(string $novoCargo)
+    {
+        $this->cargo = $novoCargo;
+    }
+
+    public function aumentarSalarioPercentual(float $percentual)
+    {
+        if ($percentual > 0){
+            $aumento = $this->salario * ($percentual / 100);
+            $this->salario += $aumento;
+        }
+    }
+
+    public function exibeInfo()
+    {
+        return "Nome: {$this->nome}"
+            . PHP_EOL .
+            "Estoque: {$this->cargo}"
+            . PHP_EOL .
+            "Preço: R$ {$this->salario}";
+    }
+}
+
+$funcionario = new Funcionario('Oliver', 'Programador Júnior', 3_500.45);
+$funcionario->alterarCargo('Programador Senior');
+$funcionario->aumentarSalarioPercentual(2.8);
+echo $funcionario->exibeInfo();
